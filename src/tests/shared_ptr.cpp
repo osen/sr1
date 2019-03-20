a4a8d56cfbb0fcf5c0f7a4d6b1961df05f34cbdf
Karsten Pedersen <kpedersen@dae.local>
2019-03-20 14:06:59 +0000

    Added minimal implementation of std::shared_ptr. I wan't to limit the amount of freedom it provides (i.e get() private). TODO: Merge locking system with minimal implementation.

---
+++ src/tests/shared_ptr.cpp
@@ -0,0 +1,63 @@
+#include <sr1/shared_ptr>
+//#include <sr1/memory>
+#include <sr1/vector>
+#include <sr1/zero_initialized>
+#include <iostream>
+
+struct Object
+{
+  Object()
+  {
+    std::cout << "Object()" << std::endl;
+  }
+
+  ~Object()
+  {
+    std::cout << "~Object()" << std::endl;
+  }
+};
+
+struct Dummy : public Object
+{
+  Dummy()
+  {
+    std::cout << "Dummy()" << std::endl;
+  }
+
+  ~Dummy()
+  {
+    std::cout << "~Dummy()" << std::endl;
+  }
+
+  std::sr1::zero_initialized<int> dummy;
+};
+
+int main()
+{
+  std::sr1::shared_ptr<Dummy> d = std::sr1::make_shared<Dummy>();
+  d = d;
+  std::sr1::shared_ptr<Dummy> d2 = d;
+  std::sr1::shared_ptr<Dummy> d3(d2);
+  d->dummy = 9;
+
+  std::sr1::shared_ptr<Dummy> dn;
+  d3 = dn;
+
+  d2 = d;
+  d = d2;
+  d3 = d3;
+
+  std::cout << d->dummy << std::endl;
+
+  std::sr1::vector<std::sr1::shared_ptr<Dummy> > ds;
+
+  for(size_t i = 0; i < 100; i++)
+  {
+    ds.push_back(d);
+  }
+
+  std::sr1::shared_ptr<Object> o = d;
+  o = d;
+
+  return 0;
+}
